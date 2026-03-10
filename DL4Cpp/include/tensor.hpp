1e498a775cfd78a354a0b34361668634bfb49426
SallyFace <saltedc137@gmail.com>
2026-03-10 23:00:05 +0800

    Create Tensor

--- DL4Cpp/include/tensor.hpp
+++ DL4Cpp/include/tensor.hpp
@@ -6,6 +6,7 @@
 
 #include <armadillo>
 
+#include <cstdint>
 #include <memory>
 #include <vector>
 
@@ -26,60 +27,62 @@ namespace dlc_inf
     template<>
     class Tensor<float>
     {
-        public:
-            explicit Tensor() = default;
+    public:
+      explicit Tensor() = default;
+      
+      /**
+       * @brief Generate a 3D tensor
+       * @param channels the number of channels in the tensor
+       * @param rows the number of rows in the tensor
+       * @param cols the number of columns in the tensor
+       */
 
-            /**
-             * @brief Generate a 3D tensor
-             * @param channels the number of channels in the tensor
-             * @param rows the number of rows in the tensor
-             * @param cols the number of columns in the tensor
-             */
+      explicit Tensor(uint32_t channels, uint32_t rows, uint32_t cols);
 
-            explicit Tensor(uint32_t channels, uint32_t rows, uint32_t cols);
+      explicit Tensor(uint32_t size);
 
-            explicit Tensor(uint32_t size);
+      explicit Tensor(uint32_t rows, uint32_t cols);
 
-            explicit Tensor(uint32_t rows, uint32_t cols);
+      explicit Tensor(const std::vector<uint32_t> &shapes);
 
-            explicit Tensor(const std::vector<uint32_t> &shapes);
-
-            /**
-             * @brief Copy constructor: deep copy the data from another tensor
-             */ 
-            Tensor(const Tensor &tensor);
+      /**
+       * @brief Copy constructor: deep copy the data from another tensor
+       */ 
+      Tensor(const Tensor &tensor);
             
-            Tensor<float> &operator=(const Tensor &tensor);
+      Tensor<float> &operator=(const Tensor &tensor);
             
             
-            /**
-             * @brief Move constructor: take over the data from another temporary 
-             *        to avoid data duplication and improve performance
-             */
-            Tensor(Tensor &&tensor) noexcept;
+      /**
+       * @brief Move constructor: take over the data from another temporary 
+       *        to avoid data duplication and improve performance
+       */
+      Tensor(Tensor &&tensor) noexcept;
+      
+      Tensor<float> &operator=(Tensor &&tensor);
 
-            Tensor<float> &operator=(Tensor &&tensor);
+      uint32_t rows() const;
+      uint32_t cols() const;
+      uint32_t channels() const;
+      uint32_t size() const;
+      bool empty() const;
 
-            uint32_t rows() const;
-            uint32_t cols() const;
-            uint32_t channels() const;
-            uint32_t size() const;
-            bool empty() const;
+      std::vector<uint32_t> shapes() const;
+      const std::vector<uint32_t> sub_shape() const;
 
-            std::vector<uint32_t> shapes() const;
-            const std::vector<uint32_t> sub_shape() const;
+      void set_data(const arma::fcube &data);
 
-            void set_data(const arma::fcube &data);
+      // 1D linear index visit
+      float index(uint32_t position) const;
+      float &index(uint32_t position);
+      
+      // 2D longitude and latitude visit
+      float posi(uint32_t rows, uint32_t cols) const;
+      float &posi(uint32_t rows, uint32_t cols);
 
-            // 1D linear index visit
-            float index(uint32_t position) const;
-            float &index(uint32_t position);
-
-            // 2D longitude and latitude visit
-            float posi(uint32_t rows, uint32_t cols) const;
-            float &posi(uint32_t rows, uint32_t cols);
-
-            
-
+      // 3D channels x y
+      float at(uint32_t channels , uint32_t rows, uint32_t cols) const;
+      float &at(uint32_t channels , uint32_t rows, uint32_t cols);
+     
     };
 }
