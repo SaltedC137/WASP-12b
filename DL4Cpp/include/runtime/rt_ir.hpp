e38f698e4bdacc6334cd117f19d484199f802456
Aska Lyn <saltedc137@gmail.com>
2026-03-18 09:31:44 +0800

    Daily Update

--- DL4Cpp/include/runtime/rt_ir.hpp
+++ DL4Cpp/include/runtime/rt_ir.hpp
@@ -13,18 +13,47 @@ namespace ctl {
 class RuntimeGraph {
 
 public:
+
+
+  // Constructor
   RuntimeGraph(std::string param_path, std::string bin_path);
 
 
-  std::vector<ften> get_output(const std::string& output_name , const std::shared_ptr<sften> input_tensors);
-
-  
+  std::vector<ften> get_output(const std::string& output_name);
 
 
+  void set_inputs(const std::string& input_name, const std::vector<sften>& inputs);
+
+
+  bool is_input_op(const std::string& op_name) const;
+
+  bool is_output_op(const std::string& op_name) const;
 
   void Build();
 
+  void param_path(const std::string& param_path);
+
+  void bin_path(const std::string& bin_path);
+
+  const std::string& param_path() const;
+
+  const std::string& bin_path() const;
+
+  void Forward(bool debug = false);
+
 private:
+
+
+  void Init();
+
+
+  std::string param_path_;
+  std::string bin_path_;
+
+
+
+
+
 };
 
 } // namespace ctl
