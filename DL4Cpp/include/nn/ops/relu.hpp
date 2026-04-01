8b47ca6018ca3c7220006a705f2e1a48e84ac0d1
ArcadeFire <16298975+arcadefire@user.noreply.gitee.com>
2026-04-01 21:47:43 +0800

    Adds ReLU activation layer header
    Introduces the header file for the ReLU activation layer, which will be used to apply the rectified linear unit activation function in neural networks. This is a foundational component for building activation layers within the framework, establishing the class structure that inherits from a common activation base class.
    
    Sets up the basic class declaration to be implemented later, ensuring proper namespace organization and include guards.

---
+++ DL4Cpp/include/nn/ops/relu.hpp
@@ -0,0 +1,31 @@
+
+
+#ifndef RELU_HPP
+
+#define RELU_HPP
+
+
+
+#include "nn/ops/activation.hpp"
+
+namespace ctl {
+namespace nn {
+
+
+
+
+class ReLU : public nn::ActivationLayer {
+public:
+  explicit ReLU();
+  
+
+
+};
+
+
+} 
+}
+
+
+
+#endif
